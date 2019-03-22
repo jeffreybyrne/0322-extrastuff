c6e5322a17dfdc10eda21479adc1d72ca00cb35f
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-22 14:10:51 -0400

    converted the first to from python to js

---
+++ exercise.js
@@ -0,0 +1,25 @@
+// apples = 14
+// print(apples)
+//
+// print("I have {} apples.".format(apples))
+
+const apples = 14;
+
+console.log(apples);
+console.log(`I have ${apples} apples.`);
+
+// materials = ['wood', 'metal', 'stone']
+// words = {
+//   'elephant': "The world's largest land mammal.",
+//   'school': 'A place of learning.',
+//   'ice cream': 'A delicious milk-based dessert.',
+// }
+
+const materials = ['wood', 'metal', 'stone'];
+const words = {
+  elephant: "The world's largest land mammal.",
+  school: "A place of learning.",
+  'ice cream': "A delicious milk-based dessert."
+}
+console.log(materials)
+console.log(words)
